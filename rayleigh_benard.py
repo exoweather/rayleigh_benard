7e6149b87ecbc6d4ff859a65c9e8540382e7ed70
Benjamin Brown <bpbrown@astro.wisc.edu>
2017-03-23 12:12:49 -0600

    Removing unused files; adding verbose keyword for sparsity pattern outputs.

--- rayleigh_benard.py
+++ rayleigh_benard.py
@@ -15,6 +15,7 @@ Options:
     --aspect=<aspect>          Aspect ratio of problem [default: 4]
     --restart=<restart_file>   Restart from checkpoint
     --label=<label>            Optional additional case name label
+    --verbose                  Do verbose output (e.g., sparsity patterns of arrays)
     
 """
 import logging
@@ -224,19 +225,20 @@ def Rayleigh_Benard(Rayleigh=1e6, Prandtl=1, nz=64, nx=None, aspect=4, restart=N
             log_string += 'Time: {:8.3e}, dt: {:8.3e}, '.format(solver.sim_time, dt)
             log_string += 'Re: {:8.3e}/{:8.3e}'.format(flow.grid_average('Re'), flow.max('Re'))
             logger.info(log_string)
+            
             if first_step:
-                import scipy.sparse.linalg as sla
-                LU = sla.splu(solver.pencils[0].LHS.tocsc(), permc_spec='NATURAL')
-                fig = plt.figure()
-                ax = fig.add_subplot(1,2,1)
-                ax.spy(LU.L.A, markersize=1, markeredgewidth=0.0)
-                ax = fig.add_subplot(1,2,2)
-                ax.spy(LU.U.A, markersize=1, markeredgewidth=0.0)
-                fig.savefig("sparsity_pattern_LU.png", dpi=1200)
-                logger.info("{} nonzero entries in LU".format(LU.nnz))
-                logger.info("{} nonzero entries in LHS".format(solver.pencils[0].LHS.tocsc().nnz))
-                logger.info("{} fill in factor".format(LU.nnz/solver.pencils[0].LHS.tocsc().nnz))
-
+                if verbose:
+                    import scipy.sparse.linalg as sla
+                    LU = sla.splu(solver.pencils[0].LHS.tocsc(), permc_spec='NATURAL')
+                    fig = plt.figure()
+                    ax = fig.add_subplot(1,2,1)
+                    ax.spy(LU.L.A, markersize=1, markeredgewidth=0.0)
+                    ax = fig.add_subplot(1,2,2)
+                    ax.spy(LU.U.A, markersize=1, markeredgewidth=0.0)
+                    fig.savefig("sparsity_pattern_LU.png", dpi=1200)
+                    logger.info("{} nonzero entries in LU".format(LU.nnz))
+                    logger.info("{} nonzero entries in LHS".format(solver.pencils[0].LHS.tocsc().nnz))
+                    logger.info("{} fill in factor".format(LU.nnz/solver.pencils[0].LHS.tocsc().nnz))
                 first_step=False
     except:
         logger.error('Exception raised, triggering end of main loop.')
@@ -281,6 +283,6 @@ if __name__ == "__main__":
                     aspect=int(args['--aspect']),
                     nz=int(args['--nz']),
                     nx=nx,
-                    data_dir=data_dir)
+                    data_dir=data_dir, vebose=args['--verbose'])
     
 
